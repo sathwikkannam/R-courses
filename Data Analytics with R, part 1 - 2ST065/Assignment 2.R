de3ef23b41f218432b499f727d5308f992844ffa
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-11-03 16:46:16 +0100

    Answer questions

--- Data Analytics with R, part 1 - 2ST065/Assignment 2.R
+++ Data Analytics with R, part 1 - 2ST065/Assignment 2.R
@@ -2,25 +2,62 @@ library(ggplot2)
 
 # 1. Correlations
 correlations <- cor(iris[, 1:4])
+# Are the correlations positive or negative? Does this make sense to you?
+
+# The correlations between Sepal.Length and Sepal.Width, Sepal.Length and Petal.Length, and Sepal.Length and Petal.Width are all positive,
+# while the correlation between Sepal.Width and Petal.Length and Sepal.Width and Petal.Width are both negative.
+# These correlations make sense because Sepal.Length and Petal.Length, as well as Sepal.Length and Petal.Width, are both positively correlated,
+# Similarly, Sepal.Width and Petal.Length, as well as Sepal.Width and Petal.Width, are both negatively correlated,
 
 # 2. Plot Sepal.Width against Sepal.Length
 plot1 <- ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length)) + geom_point()
+# How does the plotted points match the correlations we computed before?
+
+# The plot shows that as Sepal.Width increases, Sepal.Length decreases,
+# which is consistent with the negative correlation coefficient of -0.1175698
 
 # 3. Fit a linear model using Sepal.Width as predictor and Sepal.Length as response
 model1 <- lm(Sepal.Length ~ Sepal.Width, data = iris)
+# Does the estimated coefficient for sepal width match what have seen in tasks 1 and 2?
+
+# The estimated coefficient for Sepal.Width in model1 is negative, which is consistent with the negative correlation coefficient we found in Task 1 and the negative relationship we observed in Task 2.
+# However, the magnitude of the coefficient in model1 is larger than the correlation coefficient in Task 1, which suggests that the relationship between Sepal.Width and Sepal.Length may not be perfectly linear.
 
 # 4. Setosa correlations
 correlations_setosa <- cor(subset(iris, Species == "setosa")[, 1:4])
+# Are these correlations positive or negative? How are they different from the overall correlations? Does this make sense?
+
+# The correlations between Sepal.Length and Sepal.Width, Sepal.Length and Petal.Length, and Sepal.Length and Petal.Width are all positive,
+# while the correlation between Sepal.Width and Petal.Length and Sepal.Width and Petal.Width are both negative.
+# These correlations are different from the overall correlations we found earlier because they are specific to the setosa species,
+# while the overall correlations were calculated using all the species in the dataset.
 
 # 5. Plot Sepal.Width against Sepal.Length, color by species
 plot2 <- ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, color = Species)) + geom_point()
+# Does this match the correlations we computed for setosa flowers?
+
+# It matches the correlations as Sepal.Width and Sepal.Length are positively correlated with 0.7425467, hence, as Sepal.Width increases
+# Sepal.Length also increases. Which is what see in plot2 - a almost lineary correlation.
 
 # 6. Fit second model using species and Sepal.Width as predictors and Sepal.Length as response
 model2 <- lm(Sepal.Length ~ Sepal.Width + Species, data = iris)
+# Does the estimated coefficient for Sepal.Width make sense? What happened when we added species to our model? How does it change the interpretation?
+
+# Adding variable Species to the model allows us to estimate the effect of species on the relationship between Sepal.Width and Sepal.Length.
+# The estimated coefficient for Sepal.Width in model2 is 0.8036, which means that for every one unit increase in Sepal.Width, we expect an increase of 0.8036 units in Sepal.Length.
+# The coefficients for the versicolor and virginica species are both positive, which means that these species have a higher average Sepal.Length than the setosa species,
+# Specifically, the estimated coefficient for versicolor is 1.4587, which means that on average, versicolor flowers have a Sepal.Length that is 1.4587 units higher than setosa flowers.
+# Similarly, the estimated coefficient for virginica is 1.9468, which means that on average, virginica flowers have a Sepal.Length that is 1.9468 units higher than setosa flowers.
+# Adding Species to the model changes the interpretation of the coefficient for Sepal.Width, as the coefficient now represents the effect of Sepal.Width on Sepal.Length for a given species.
+# Therefore allowing us to estimate the average difference in Sepal.Length between the species.
 
 # 7. Predict the sepal length of a setosa with a sepal width of 3.6 cm
 newdata <- data.frame(Sepal.Width = 3.6, Species = "setosa")
 prediction <- predict(model2, newdata)
+# Does the prediction appear reasonable?
+
+# It is resonable as the prediction is within the range of Sepal.Length.
+# From plot2 we can see that Sepal.Length at ~3.5 results in a Sepal.Length ~5.
 
 # 8. Load the Pima Indian diabetes .csv into R
 diabetes_data <- read.csv("a2_diabetes.csv")
@@ -30,5 +67,4 @@ logistic_model <- glm(Outcome ~ ., data = diabetes_data, family = binomial)
 
 # 10. Compute the accuracy
 predicted_values <- ifelse(predict(logistic_model, type = "response") > 0.5, 1, 0)
-actual_values <- diabetes_data$Outcome
-accuracy <- sum(predicted_values == actual_values) / nrow(diabetes_data)
+accuracy <- sum(predicted_values == diabetes_data$Outcome) / nrow(diabetes_data)
